6a777d0e2cf6e92b987d93b5c6f62cfe32ad597f
AndyTums <tumerrkin@gmail.com>
2025-05-01 16:38:48 +0300

    add models users + db postgres

---
+++ shop/urls.py
@@ -0,0 +1,17 @@
+# from django.conf.urls.static import static
+# from rest_framework.routers import DefaultRouter
+#
+# from config import settings
+# from users.apps import UsersConfig
+#
+# app_name = UsersConfig.name
+#
+# router = DefaultRouter()
+# router.register("", UserViewSet, basename="users")
+#
+# urlpatterns = []
+#
+# urlpatterns += router.urls
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
